d638a917d51e77d85dff98fd6b808f417f4c8fce
Danilo Carrasco <danilo.carrasco.95@gmail.com>
2022-07-13 23:18:33 -0400

    Agregado en el encabezado se la sección resultados la palabra clave buscada, el número de productos encontrados y el número de productos filtrados

--- script.js
+++ script.js
@@ -9,20 +9,22 @@ $(async () => {
     const categories = await getCategories();
     let products = await getProducts();
 
-    // Se crea los filtros de las categorias, las cartas de todos los productos y los números de productos por filtros.
+    // Se crea los filtros de las categorias, las cartas de todos los productos y los números de productos por filtros al cargar la página.
     $(".categorias").html(createCategoryFilter(categories));
+    infoResults(products);
     createCards(products);
     productsForCategoriesAndPriceRange(categories, products);
-
-
+    
+    
     // LISTENERS
-
+    
     //// Escucha el evento submit del formulario de búsqueda. Limpia los filtros, realizando una consulta de los productos a la API y muestra los productos en pantalla.
     $(".form-buscar").on("submit", async function (event) {
         event.preventDefault();
         cleanFilters();
-
+        
         products = await getProducts();
+        infoResults(products);
         createCards(products);
         productsForCategoriesAndPriceRange(categories, products);
     })
@@ -32,24 +34,28 @@ $(async () => {
         cleanFilters();
 
         products = await getProducts();
+        infoResults(products);
         createCards(products);
     })
 
     //// Escucha si hay cambios en los filtros de rango de precios. Realiza una consulta de los productos a la API con los filtros seleccionados y muestra los productos en pantalla.
     $("#menor-1500, #entre-1500-5000, #mayor-5000").on("change", async function (){
         products = await getProducts();
+        infoFilters(products);
         createCards(products);
     })
 
     //// Escucha si hay cambios en el selector de ordenar por. Realiza una consulta de los productos a la API con el orden seleccionado y muestra los productos en pantalla.
     $("#orden").on("change", async function (){  
         products = await getProducts();
+        infoFilters(products);
         createCards(products);
     })
 
     //// Escucha si hay cambios en los filtros de categorias. Realiza una consulta de los productos a la API con los filtros seleccionados y muestra los productos en pantalla.
     $(".categorias input").on("change", async function (){  
         products = await getProducts();
+        infoFilters(products);
         createCards(products);
     })
 
@@ -112,7 +118,32 @@ const productsForCategoriesAndPriceRange = async (categories, products) => {
 
 //// Limpia los filtros, deseleccionando los checkbox de los filtros
 //// cleanFilters(): void
-var cleanFilters = () => {
+const cleanFilters = () => {
     $(".categorias .form-check-input").prop("checked", false);
     $(".rango-precios .form-check-input").prop("checked", false);
 };
+
+
+const infoResults = (products) => {
+
+    const search = $("#busqueda").val();
+    const quantityOfResults = products.length;
+
+    const resultsTitle = search ? ` de '${search}'` : "";
+    const quantityTitle = `${quantityOfResults} productos`;
+
+    $("#titulo-resultados").html(resultsTitle);
+    $("#cantidad-resultados").html(quantityTitle);
+    $("#filtrados-resultados").html("");
+
+};
+
+const infoFilters = (products) => {
+
+    const quantityOfResults = products.length;
+
+    const filterTitle = quantityOfResults ? ` y ${quantityOfResults} filtrados` : "";
+
+    $("#filtrados-resultados").html(filterTitle);
+
+}
